47d0e1f341b4ca1fe8f6a9eb3f6b3c7f8b26f700
Matthijs Kralt <matthijs@webinargeek.com>
2024-03-01 00:44:13 +0800

    initial landing page work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,53 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="flex flex-col items-center px-4 py-6 text-center">
+	<h1 class="mb-8">Do it or else...</h1>
+	<h2 class="mb-8">Link MASSIVE PAIN to not following through on your goals</h2>
+	<form action="" class="">
+		<label for="goal" class="mb-2 block">What is your goal?</label>
+
+		<textarea
+			placeholder="I will..."
+			id="goal"
+			name="goal"
+			class="mb-4 block h-20 w-full rounded-lg border-gray-600 bg-gray-900 px-4 py-2.5 text-gray-300 placeholder-gray-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+		></textarea>
+
+		<button
+			class="transform rounded-lg bg-red-600 px-6 py-2 font-medium capitalize tracking-wide text-white transition-colors duration-300 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+		>
+			Hold me accountable
+		</button>
+	</form>
+</section>
+
+<section class="px-4 py-6">
+	<h2 class="mb-4 text-center text-3xl font-bold">
+		Or else? What will happen if you don't follow through?
+	</h2>
+	<ul class="grid gap-4">
+		<li class="rounded-lg border border-gray-800 p-4">
+			<h2 class="mb-2">Donate to Trump's campaign</h2>
+			<p>
+				Donald Trump, the 45th U.S. President, is known for his unique, and sometimes controversial
+				political style. This year he's running again, focusing on strict immigration rules, less
+				regulation, and putting America first.
+			</p>
+			<p>
+				Choose this as your consequence of not following through if you don't support Trump and
+				don't want him to become president again.
+			</p>
+			<img src="/images/trumpie.jpg" alt="Trump on stage" />
+			<button
+				class="transform rounded-lg bg-red-600 px-6 py-2 font-medium capitalize tracking-wide text-white transition-colors duration-300 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+			>
+				Donate to Trump if I don't follow through
+			</button>
+		</li>
+
+		<li class="rounded-lg border border-gray-800 p-4">
+			<h2 class="mb-2">Donate to Biden's campaign</h2>
+			<p>Joseph Biden, the 46th U.S. President, ...</p>
+			<p>Choose this if you don't support Biden and don't want him to become president again.</p>
+			<img src="/images/biden.jpg" alt="Trump on stage" />
+		</li>
+	</ul>
+</section>
