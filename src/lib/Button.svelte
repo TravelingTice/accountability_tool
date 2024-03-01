cd33d3b0ac915861ed01f86f1322339295339244
Matthijs Kralt <matthijs@webinargeek.com>
2024-03-01 14:56:52 +0800

    complete consequences list

---
+++ src/lib/Button.svelte
@@ -0,0 +1,7 @@
+<button {...$$restProps}><slot /></button>
+
+<style>
+	button {
+		@apply transform rounded-lg bg-red-600 px-6 py-2 font-medium capitalize tracking-wide text-white transition-colors duration-300 hover:bg-red-500 focus:outline-none focus:ring focus:ring-red-300 focus:ring-opacity-80;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import Button from '$lib/Button.svelte';
+</script>
+
 <section class="flex flex-col items-center px-4 py-6 text-center">
 	<h1 class="mb-8">Do it or else...</h1>
 	<h2 class="mb-8">Link MASSIVE PAIN to not following through on your goals</h2>
@@ -8,46 +12,159 @@
 			placeholder="I will..."
 			id="goal"
 			name="goal"
-			class="mb-4 block h-20 w-full rounded-lg border-gray-600 bg-gray-900 px-4 py-2.5 text-gray-300 placeholder-gray-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+			class="mb-4 block h-20 w-full resize-none rounded-lg border-gray-600 bg-gray-900 px-4 py-2.5 text-gray-300 placeholder-gray-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
 		></textarea>
 
-		<button
-			class="transform rounded-lg bg-red-600 px-6 py-2 font-medium capitalize tracking-wide text-white transition-colors duration-300 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
-		>
-			Hold me accountable
-		</button>
+		<Button>Hold me accountable</Button>
 	</form>
 </section>
 
 <section class="px-4 py-6">
-	<h2 class="mb-4 text-center text-3xl font-bold">
-		Or else? What will happen if you don't follow through?
-	</h2>
+	<h2 class="mb-4 text-center text-3xl font-bold">What will happen if you don't follow through?</h2>
 	<ul class="grid gap-4">
-		<li class="rounded-lg border border-gray-800 p-4">
-			<h2 class="mb-2">Donate to Trump's campaign</h2>
-			<p>
-				Donald Trump, the 45th U.S. President, is known for his unique, and sometimes controversial
-				political style. This year he's running again, focusing on strict immigration rules, less
-				regulation, and putting America first.
-			</p>
-			<p>
-				Choose this as your consequence of not following through if you don't support Trump and
-				don't want him to become president again.
-			</p>
-			<img src="/images/trumpie.jpg" alt="Trump on stage" />
-			<button
-				class="transform rounded-lg bg-red-600 px-6 py-2 font-medium capitalize tracking-wide text-white transition-colors duration-300 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
-			>
-				Donate to Trump if I don't follow through
-			</button>
+		<li class="space-y-4 rounded-lg border border-gray-800 p-4">
+			<h2>Donate to Trump's presidential campaign</h2>
+			<div class="space-y-2">
+				<p>
+					Donald Trump, the 45th U.S. President, is known for his unique, and sometimes
+					controversial political style. This year he's running again, focusing on strict
+					immigration rules, less regulation, and putting America first.
+				</p>
+				<p>
+					<strong>
+						Choose this as your consequence of not following through if you don't support Trump and
+						don't want him to become president again.
+					</strong>
+				</p>
+			</div>
+			<div class="flex w-full justify-center">
+				<div class="relative mb-2 mt-6">
+					<div class="absolute -right-14 -top-8 rotate-6 rounded-full bg-white p-4">
+						<img class="h-16 w-16" src="/images/gop.png" alt="Republican Party logo" />
+					</div>
+					<img
+						class="h-48 w-48 rounded-full object-cover"
+						src="/images/trumpie.jpg"
+						alt="Trump on stage"
+					/>
+				</div>
+			</div>
+
+			<Button>Donate to Trump if I don't follow through</Button>
 		</li>
 
-		<li class="rounded-lg border border-gray-800 p-4">
-			<h2 class="mb-2">Donate to Biden's campaign</h2>
-			<p>Joseph Biden, the 46th U.S. President, ...</p>
-			<p>Choose this if you don't support Biden and don't want him to become president again.</p>
-			<img src="/images/biden.jpg" alt="Trump on stage" />
+		<li class="space-y-4 rounded-lg border border-gray-800 p-4">
+			<h2>Donate to Biden's presidential campaign</h2>
+			<div class="space-y-2">
+				<p>
+					Joe Biden, the 46th President of the United States, focuses on unity, healthcare
+					improvement, and climate action. He supports stronger alliances abroad and policies for
+					middle-class growth. Biden uses a more traditional approach to communicate and govern.
+				</p>
+				<p>
+					<strong>
+						Choose this as your consequence of not following through if you don't support Biden and
+						don't want him to become president again.
+					</strong>
+				</p>
+			</div>
+			<div class="flex w-full justify-center">
+				<div class="relative mb-2 mt-6">
+					<div class="absolute -right-14 -top-8 rotate-6 rounded-full bg-white p-4">
+						<img class="h-16 w-16" src="/images/dplogo.svg" alt="Republican Party logo" />
+					</div>
+					<img
+						class="h-48 w-48 rounded-full object-cover"
+						src="/images/joetje.webp"
+						alt="Joe Biden on stage"
+					/>
+				</div>
+			</div>
+
+			<Button>Donate to Biden if I don't follow through</Button>
+		</li>
+
+		<li class="space-y-4 rounded-lg border border-gray-800 p-4">
+			<h2>Donate to the NRA (National Rifle Association)</h2>
+			<div class="space-y-2">
+				<p>
+					The National Rifle Association (NRA) is a powerful organization in the United States that
+					advocates for gun rights. It focuses on defending the Second Amendment, which allows
+					Americans to own guns. The NRA is known for its influence on politics, lobbying against
+					gun control laws.
+				</p>
+				<p>
+					<strong>
+						Choose this as your consequence of not following through if you don't support the NRA
+						and don't stand for their values.
+					</strong>
+				</p>
+			</div>
+			<div class="flex w-full justify-center">
+				<div class="relative my-2">
+					<img class="h-36 rounded-lg object-cover" src="/images/nra.png" alt="Logo of the NRA" />
+				</div>
+			</div>
+
+			<Button>Donate to the NRA if I don't follow through</Button>
+		</li>
+
+		<li class="space-y-4 rounded-lg border border-gray-800 p-4">
+			<h2>Donate to the American Petroleum Institute</h2>
+			<div class="space-y-2">
+				<p>
+					The American Petroleum Institute (API) is a leading organization in the U.S. that
+					represents the oil and natural gas industry. It focuses on promoting the interests of its
+					members by lobbying for favorable policies and regulations. It plays a key role in shaping
+					energy policies and advocating for the use of fossil fuels.
+				</p>
+				<p>
+					<strong>
+						Choose this as your consequence of not following through if you don't support the
+						American Petroleum Institute and don't stand for their values.
+					</strong>
+				</p>
+			</div>
+			<div class="flex w-full justify-center">
+				<div class="relative my-2">
+					<img
+						class="h-32 rounded-lg object-cover"
+						src="/images/api.jpeg"
+						alt="Logo of the American Petroleum Institute"
+					/>
+				</div>
+			</div>
+
+			<Button>Donate to the API if I don't follow through</Button>
+		</li>
+
+		<li class="space-y-4 rounded-lg border border-gray-800 p-4">
+			<h2>Donate to Greenpeace</h2>
+			<div class="my-2 space-y-2">
+				<p>
+					Greenpeace is an international environmental organization known for its activism against
+					pollution, deforestation, and climate change. It campaigns for renewable energy and the
+					protection of natural habitats. Greenpeace uses direct action, research, and advocacy to
+					raise awareness and drive environmental change.
+				</p>
+				<p>
+					<strong>
+						Choose this as your consequence of not following through if you don't support Greenpeace
+						and don't stand for their values.
+					</strong>
+				</p>
+			</div>
+			<div class="flex w-full justify-center">
+				<div class="relative">
+					<img
+						class="h-36 rounded-lg object-cover"
+						src="/images/greenpeace.jpeg"
+						alt="Logo of Greenpeace"
+					/>
+				</div>
+			</div>
+
+			<Button>Donate to Greenpeace if I don't follow through</Button>
 		</li>
 	</ul>
 </section>
