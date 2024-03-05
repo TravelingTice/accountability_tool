fdf2bfc8268146908aebeb34ef167e6b4ed3b6e1
Matthijs Kralt <matthijs@webinargeek.com>
2024-03-06 00:14:53 +0800

    Revert "Attempt at making it all work with grid but screw it it's too hard, gonna use flex instead"
    This reverts commit 7e7fc96728fb52197dceb9b67d6ec0186df81604.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,13 +26,12 @@
 	</form>
 </section>
 
-<section class="overflow-hidden px-4 py-6">
+<section class="px-4 py-6">
 	<h2 class="mb-4 text-center text-3xl font-bold">What will happen if you don't follow through?</h2>
-	<ul class="consequence-list">
-		<li class="consequence-list-item">
-			<h2 class="title">Donate to Trump's presidential campaign</h2>
-
-			<div class="text">
+	<ul class="grid gap-4">
+		<li class="space-y-4 rounded-lg border border-gray-800 p-4">
+			<h2>Donate to Trump's presidential campaign</h2>
+			<div class="space-y-2">
 				<p>
 					Donald Trump, the 45th U.S. President, is known for his unique, and sometimes
 					controversial political style. This year he's running again, focusing on strict
@@ -45,26 +44,27 @@
 					</strong>
 				</p>
 			</div>
-
-			<div class="image politician">
-				<div class="image-container">
-					<div class="logo-container">
-						<img src="/images/gop.png" alt="Republican Party logo" />
+			<div class="flex w-full justify-center">
+				<div class="relative mb-2 mt-6">
+					<div class="absolute -right-14 -top-8 rotate-6 rounded-full bg-white p-4">
+						<img class="h-16 w-16" src="/images/gop.png" alt="Republican Party logo" />
 					</div>
-					<img class="face-image" src="/images/trumpie.jpg" alt="Trump on stage" />
+					<img
+						class="h-48 w-48 rounded-full object-cover"
+						src="/images/trumpie.jpg"
+						alt="Trump on stage"
+					/>
 				</div>
 			</div>
 
-			<div class="button-container">
-				<Button on:click={handleChooseConsequence('trump')}
-					>Donate to Trump if I don't follow through</Button
-				>
-			</div>
+			<Button on:click={handleChooseConsequence('trump')}
+				>Donate to Trump if I don't follow through</Button
+			>
 		</li>
 
-		<li class="consequence-list-item">
-			<h2 class="title">Donate to Biden's presidential campaign</h2>
-			<div class="text">
+		<li class="space-y-4 rounded-lg border border-gray-800 p-4">
+			<h2>Donate to Biden's presidential campaign</h2>
+			<div class="space-y-2">
 				<p>
 					Joe Biden, the 46th President of the United States, focuses on unity, healthcare
 					improvement, and climate action. He supports stronger alliances abroad and policies for
@@ -77,25 +77,27 @@
 					</strong>
 				</p>
 			</div>
-			<div class="image politician">
-				<div class="image-container">
-					<div class="logo-container">
-						<img src="/images/dplogo.svg" alt="Republican Party logo" />
+			<div class="flex w-full justify-center">
+				<div class="relative mb-2 mt-6">
+					<div class="absolute -right-14 -top-8 rotate-6 rounded-full bg-white p-4">
+						<img class="h-16 w-16" src="/images/dplogo.svg" alt="Republican Party logo" />
 					</div>
-					<img class="face-image" src="/images/joetje.webp" alt="Joe Biden on stage" />
+					<img
+						class="h-48 w-48 rounded-full object-cover"
+						src="/images/joetje.webp"
+						alt="Joe Biden on stage"
+					/>
 				</div>
 			</div>
 
-			<div class="button-container">
-				<Button on:click={handleChooseConsequence('biden')}
-					>Donate to Biden if I don't follow through</Button
-				>
-			</div>
+			<Button on:click={handleChooseConsequence('biden')}
+				>Donate to Biden if I don't follow through</Button
+			>
 		</li>
 
-		<li class="consequence-list-item">
-			<h2 class="title">Donate to the NRA (National Rifle Association)</h2>
-			<div class="text">
+		<li class="space-y-4 rounded-lg border border-gray-800 p-4">
+			<h2>Donate to the NRA (National Rifle Association)</h2>
+			<div class="space-y-2">
 				<p>
 					The National Rifle Association (NRA) is a powerful organization in the United States that
 					advocates for gun rights. It focuses on defending the Second Amendment, which allows
@@ -109,22 +111,20 @@
 					</strong>
 				</p>
 			</div>
-			<div class="image logo">
+			<div class="flex w-full justify-center">
 				<div class="relative my-2">
 					<img class="h-36 rounded-lg object-cover" src="/images/nra.png" alt="Logo of the NRA" />
 				</div>
 			</div>
 
-			<div class="button-container">
-				<Button on:click={handleChooseConsequence('nra')}
-					>Donate to the NRA if I don't follow through</Button
-				>
-			</div>
+			<Button on:click={handleChooseConsequence('nra')}
+				>Donate to the NRA if I don't follow through</Button
+			>
 		</li>
 
-		<li class="consequence-list-item">
-			<h2 class="title">Donate to the American Petroleum Institute</h2>
-			<div class="text">
+		<li class="space-y-4 rounded-lg border border-gray-800 p-4">
+			<h2>Donate to the American Petroleum Institute</h2>
+			<div class="space-y-2">
 				<p>
 					The American Petroleum Institute (API) is a leading organization in the U.S. that
 					represents the oil and natural gas industry. It focuses on promoting the interests of its
@@ -138,7 +138,7 @@
 					</strong>
 				</p>
 			</div>
-			<div class="image logo">
+			<div class="flex w-full justify-center">
 				<div class="relative my-2">
 					<img
 						class="h-32 rounded-lg object-cover"
@@ -148,16 +148,14 @@
 				</div>
 			</div>
 
-			<div class="button-container">
-				<Button on:click={handleChooseConsequence('api')}
-					>Donate to the API if I don't follow through</Button
-				>
-			</div>
+			<Button on:click={handleChooseConsequence('api')}
+				>Donate to the API if I don't follow through</Button
+			>
 		</li>
 
-		<li class="consequence-list-item">
-			<h2 class="title">Donate to Greenpeace</h2>
-			<div class="text">
+		<li class="space-y-4 rounded-lg border border-gray-800 p-4">
+			<h2>Donate to Greenpeace</h2>
+			<div class="my-2 space-y-2">
 				<p>
 					Greenpeace is an international environmental organization known for its activism against
 					pollution, deforestation, and climate change. It campaigns for renewable energy and the
@@ -171,7 +169,7 @@
 					</strong>
 				</p>
 			</div>
-			<div class="image logo">
+			<div class="flex w-full justify-center">
 				<div class="relative">
 					<img
 						class="h-36 rounded-lg object-cover"
@@ -181,93 +179,11 @@
 				</div>
 			</div>
 
-			<div class="button-container">
-				<Button on:click={handleChooseConsequence('greenpeace')}
-					>Donate to Greenpeace if I don't follow through</Button
-				>
-			</div>
+			<Button on:click={handleChooseConsequence('greenpeace')}
+				>Donate to Greenpeace if I don't follow through</Button
+			>
 		</li>
 	</ul>
 </section>
 
 <CreatePledge />
-
-<style>
-	.consequence-list {
-		@apply flex flex-col gap-4 md:gap-6;
-	}
-
-	.consequence-list-item {
-		@apply mx-auto max-w-md space-y-4 rounded-lg border border-gray-800 p-4 md:max-w-none;
-		@apply md:space-y-2;
-		@media (min-width: 768px) {
-			display: grid;
-			grid-template-areas:
-				'title image'
-				'text image'
-				'button image';
-			grid-template-columns: 1fr auto;
-			grid-gap: 1rem;
-		}
-	}
-
-	.consequence-list-item:nth-child(even) {
-		@media (min-width: 768px) {
-			grid-template-areas:
-				'image title'
-				'image text'
-				'image button';
-			grid-template-columns: auto 1fr;
-		}
-	}
-
-	.consequence-list-item .title {
-		grid-area: title;
-	}
-
-	.consequence-list-item .text {
-		grid-area: text;
-		@apply space-y-2 md:max-w-sm;
-	}
-
-	.consequence-list-item:nth-child(even) .text {
-		@apply md:place-self-end;
-	}
-
-	.consequence-list-item .image {
-		grid-area: image;
-		place-self: center;
-		@apply flex w-full justify-center;
-	}
-
-	.consequence-list-item .image.politician .image-container {
-		@apply relative mb-2 mt-6 md:mr-8;
-	}
-
-	.consequence-list-item .image.politician .logo-container {
-		@apply absolute -right-14 -top-8 rotate-6 rounded-full bg-white p-4 md:-right-8;
-	}
-
-	.consequence-list-item .image.politician .face-image {
-		@apply h-48 w-48 rounded-full object-cover md:h-40 md:w-40;
-	}
-
-	.consequence-list-item .image.politician .logo-container img {
-		@apply h-16 w-16 md:h-12 md:w-12;
-	}
-
-	.consequence-list-item .image.logo img {
-		@apply w-48 md:h-auto;
-	}
-
-	.consequence-list-item .button-container {
-		grid-area: button;
-		@apply flex justify-center md:justify-start;
-	}
-
-	.consequence-list-item:nth-child(even) .button-container {
-		@media (min-width: 768px) {
-			justify-content: flex-end;
-		}
-	}
-</style>
